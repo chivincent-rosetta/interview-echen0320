22a35956b775dfca981ac1371946955eec5b73cb
Eric <chen_yiting2003@hotmail.com>
2021-01-15 15:14:12 +0800

    Almost there

--- index.php
+++ index.php
@@ -15,38 +15,57 @@ class xmlString {
 
 //Create $xml first, then use fromXML to load the string.
 public static $xml;
+public static $xmldoc;
 public $xmlobject;
 public static $strxml;
 public static $xmldata;
 
   public static function fromXMLFile($source){
-    $xml = new DomDocument();
-    $xml->load($source);
+    self::$xmldoc = new DomDocument();
+    self::$xmldoc->load($source);
     //$xmlobject = simplexml_load_file($source);
     //self::$xml = $xmlobject->asXML();
     //print_r($xml) previously
-    print_r($xml->savexml());
+    self::$xml = self::$xmldoc->savexml();
+    //echo $xml;
   }
 //Create the domxml object for future use
   public static function fromXML($xml){
     //self::$strxml = file_get_contents($xml);
-    self::$xmldata = simplexml_load_string($xml);
-    echo self::$xmldata::_toString();
-  }
+    // echo self::$xmldoc != null;
 
+    $bool = self::$xmldata = self::$xmldoc->loadXML($xml);
+    $newXML = self::$xmldoc->saveXML();
+    // echo $newXML;
+  }
+  //Save to file
   public function toXML(){
 
   }
+  //This can probably also be done with xpath
+  //public function gettitle(){
+  //  $this->;
+  //}
+  public function settitle($title,$newtitle){
+    //Find out how escape characters work
+    $xpath = new DOMXpath(self::$xmldoc);
+    $changes = $xpath->query("channel/item[g:title = '$title' ]/g:title");
+    $this->title = $newtitle;
+    // echo $changes->count();
+    $DOMnode = $changes->item(0);
+    // echo $DOMnode->getNodePath();
+    echo 'DOMnode<br>' . $DOMnode->nodeValue;
+    echo 'DOMnode<br>' . $DOMnode->nodeName;
 
-  public function gettitle(){
-    $this->
-  }
-  public function settitle($title){
-    //This code attempts to look for all titles, change it to look for the specific title the function points to?
-    $changes = self::$xmldata->xpath("//*[g:title ==]");
-    $this->title = $title;
+    $DOMnode->nodeValue = $newtitle;
+    echo '<br>Xmldoc<br>' . self::$xmldoc->saveXML();
   }
 }
+
+xmlString::fromXMLFile('test2.xml');
+xmlString::fromXML(xmlString::$xml);
+$xmlString = new xmlString();
+$xmlString->settitle('《有品味的營養口糧》','有品味');
 //xmlString::fromXMLFile('test.xml');
 //$xml = simplexml_load_file('test.xml');
 
